07a5acdb1436b68e33c02524b8d11b3e39b8b56e
FengYing2000 <82642913+FengYing2000@users.noreply.github.com>
2026-09-08 14:37:00 +0800

    回到 flutter_secure_storage 11 ＋ compileSdk 37（Android build 修正）
    上一版降到 9.2.4 是死路：它的 Windows 子套件要 win32 ^5，而 share_plus／
    package_info_plus 等整串 plus 套件都要 win32 ^6；flutter build 會編所有
    平台的 plugin dart，任何單一 win32 版本都會弄壞一邊、連 iOS 一起垮。
    11 天生相容 win32 6（iOS 在 e08da00 早就編過），只差 Android compileSdk 37
    沒設。AGP 9.1.0 支援 37，補上 compileSdk 37 + minSdk 24。

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.fengying.gamemale"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = maxOf(flutter.compileSdkVersion, 37)
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -17,7 +17,9 @@ android {
     defaultConfig {
         // 跟 iOS 的 bundle id 一致
         applicationId = "com.fengying.gamemale"
-        minSdk = flutter.minSdkVersion
+        // flutter_secure_storage 11（記住密碼的加密儲存）要求 Android
+        // compileSdk 37、minSdk 24。Flutter 預設較低，取兩者較大值。
+        minSdk = maxOf(flutter.minSdkVersion, 24)
         targetSdk = flutter.targetSdkVersion
         // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
         // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
